24387dab68c18ac8e7aa3c1777950ccda3cf913b
DmitriyNosikov <deimon1993rf2@yandex.ru>
2024-04-01 23:54:54 +0300

    Реализована сущность BlogUserEntity и ее фабрика

---
+++ project/libs/user/blog-user/src/lib/blog-user.entity.ts
@@ -0,0 +1,35 @@
+import { AuthUserInterface, Entity, StorableEntity } from '@project/shared/core'
+
+export class BlogUserEntity extends Entity implements StorableEntity<AuthUserInterface> {
+  public email: string;
+  public avatar: string;
+  public date: string;
+  public passwordHash: string;
+
+  constructor(user?: AuthUserInterface) {
+    super();
+    this.populate(user);
+  }
+
+  public populate(user?: AuthUserInterface) {
+    if(!user) {
+      return;
+    }
+
+    this.id = user.id ?? '';
+    this.email = user.email;
+    this.avatar = user.avatar;
+    this.date = user.date;
+    this.passwordHash = user.passwordHash;
+  }
+
+  public toPOJO(): AuthUserInterface {
+    return {
+      id: this.id,
+      email: this.email,
+      avatar: this.avatar,
+      date: this.date,
+      passwordHash: this.passwordHash
+    };
+  }
+}
---
+++ project/libs/user/blog-user/src/lib/blog-user.factory.ts
@@ -0,0 +1,8 @@
+import { AuthUserInterface, EntityFactory } from '@project/shared/core'
+import { BlogUserEntity } from './blog-user.entity';
+
+export class BlogUserFactory implements EntityFactory<BlogUserEntity> {
+  public create(entityPlainData: AuthUserInterface): BlogUserEntity {
+    return new BlogUserEntity(entityPlainData);
+  }
+}
