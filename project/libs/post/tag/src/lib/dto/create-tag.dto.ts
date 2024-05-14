d2d9f438e3e60a429dcb0fcee00f10c65bdd3203
DmitriyNosikov <deimon1993rf2@yandex.ru>
2024-05-15 00:43:01 +0300

    Исправлена и доработана валидация тегов

--- project/libs/post/tag/src/lib/dto/create-tag.dto.ts
+++ project/libs/post/tag/src/lib/dto/create-tag.dto.ts
@@ -1,17 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, MaxLength, MinLength, IsNotEmpty } from 'class-validator';
-// import { BlogPostValidation } from '@project/blog-post' // Почему то не работает
+import { IsString, MaxLength, MinLength, IsNotEmpty, IsAlphanumeric, Matches } from 'class-validator';
+
+const Validation = {
+  NAME: {
+    MIN_LENGTH: 3,
+    MAX_LENGTH: 10,
+    MAX_СOUNT: 8
+  }
+}
+
 export class CreateTagDTO {
   @ApiProperty({
     type: [String],
     description: 'Tag name',
     example: 'tag1',
-    minLength: 3,
-    maxLength: 10,
+    minLength: Validation.NAME.MIN_LENGTH,
+    maxLength: Validation.NAME.MAX_LENGTH,
   })
-  @MinLength(3)
-  @MaxLength(10)
+  @MinLength(Validation.NAME.MIN_LENGTH)
+  @MaxLength(Validation.NAME.MAX_LENGTH)
   @IsString()
   @IsNotEmpty()
+  @Matches(new RegExp('^[a-zA-Zа-яА-Я]', 'gmi'), { message: 'tag name must starts with letter'})
+  @IsAlphanumeric('en-US')
+  @IsString()
   name: string;
 }
